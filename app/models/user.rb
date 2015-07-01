5c2c65f0c6d5c11868270db5b314a10531979c0b
TheoLeanse <theoleanse@gmail.com>
2015-07-01 15:49:40 +0100

    adds Logger to DataMapper setup file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,12 @@ require 'bcrypt'
 
 class User
   include DataMapper::Resource
-
   attr_reader :password
   attr_accessor :password_confirmation
-
   validates_confirmation_of :password
 
-  property :id, Serial
-  property :email, String
+  property :id,              Serial
+  property :email,           String
   property :password_digest, Text
 
   def password=(password)
